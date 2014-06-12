9fd2ebc5728a021ca9fc001deb75895fedf237c2
brainelectronics <info@brainelectronics.de>
2014-06-12 23:31:01 +0200

    Initial Commit

---
+++ Solar System/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Solar System
+//
+//  Created by Jonas Scharpf on 12.06.14.
+//  Copyright (c) 2014 Jonas Scharpf. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
